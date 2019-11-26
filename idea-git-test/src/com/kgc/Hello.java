c896dab5d98f57349400d59be676807a42febda3
龙阳召 <596160051@qq.com>
2019-11-26 15:58:55 +0800

    第一次修改

--- idea-git-test/src/com/kgc/Hello.java
+++ idea-git-test/src/com/kgc/Hello.java
@@ -3,7 +3,7 @@ package com.kgc;
 public class Hello {
     public static void main(String[] args) {
         String a = "aaa";
-        string b = "haha"
+        String b = "haha";
         System.out.println(a);
     }
 
